62620d395ab55661c55b22bee1492f2883568e70
IKEDA <ruizuikeda@gmail.com>
2016-11-30 09:37:48 -0200

    Alterando de Welcome para Index

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+
+
+
+
+
+
+
+
+
+
+/*
+| -------------------------------------------------------------------------
+| LOGIN
+| -------------------------------------------------------------------------
+| Rotas da tela de login
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
